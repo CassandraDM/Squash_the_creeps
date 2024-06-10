3d190a9c33c48aa623183351fd0dc6482ee9fa00
CassandraDM <cassandra.delmas-marchiset@mail-esd.com>
2024-06-10 16:09:43 +0200

    Spawning monsters

--- Main.gd
+++ Main.gd
@@ -1,34 +1,20 @@
 extends Node
 
-export(PackedScene) var mob_scene
+export var mob_scene: PackedScene
 
 
-func _ready():
-	randomize()
-	$UserInterface/Retry.hide()
+func _on_mob_timer_timeout():
+	# Create a new instance of the Mob scene.
+	var mob = mob_scene.instantiate()
 
-
-func _unhandled_input(event):
-	if event.is_action_pressed("ui_accept") and $UserInterface/Retry.visible:
-		get_tree().reload_current_scene()
-
-
-func _on_MobTimer_timeout():
-	# Create a Mob instance and add it to the scene.
-	var mob = mob_scene.instance()
-
-	# Choose a random location on Path2D.
+	# Choose a random location on the SpawnPath.
+	# We store the reference to the SpawnLocation node.
 	var mob_spawn_location = get_node("SpawnPath/SpawnLocation")
-	mob_spawn_location.unit_offset = randf()
+	# And give it a random offset.
+	mob_spawn_location.progress_ratio = randf()
 
-	var player_position = $Player.transform.origin
+	var player_position = $Player.position
+	mob.initialize(mob_spawn_location.position, player_position)
 
+	# Spawn the mob by adding it to the Main scene.
 	add_child(mob)
-	# We connect the mob to the score label to update the score upon squashing a mob.
-	mob.connect("squashed", $UserInterface/ScoreLabel, "_on_Mob_squashed")
-	mob.initialize(mob_spawn_location.translation, player_position)
-
-
-func _on_Player_hit():
-	$MobTimer.stop()
-	$UserInterface/Retry.show()
